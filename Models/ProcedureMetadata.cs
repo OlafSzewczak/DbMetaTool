2d1176d726aa518ce3663d387e4fa4b19831e8a5
Olaf Szewczak <os@sensesoft.eu>
2025-11-19 04:33:11 +0100

    Add List of ProcedureParameterMetadata type to ProcedureMetadata

--- Models/ProcedureMetadata.cs
+++ Models/ProcedureMetadata.cs
@@ -4,5 +4,6 @@
     {
         public string Name { get; set; }
         public string Source { get; set; }
+        public List<ProcedureParameter> Parameters { get; set; } = [];
     }
 }
---
+++ Models/ProcedureParameterMetadata.cs
@@ -0,0 +1,10 @@
+﻿namespace DbMetaTool.Models
+{
+    public class ProcedureParameter
+    {
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public int ParameterType { get; set; } // 0 = INPUT, 1 = OUTPUT
+        public int Position { get; set; }
+    }
+}
